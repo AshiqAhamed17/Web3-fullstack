7dfc0745e5c76fd61b7f3523db3ee78b14fdf35b
Ashiq Ahamed <aashiq121205@gmail.com>
2025-05-06 06:49:13 +0530

    fund

--- 01-Buy-me-a-coffee/index.js
+++ 01-Buy-me-a-coffee/index.js
@@ -1,15 +1,21 @@
-import { createWalletClient, custom } from "https://esm.sh/viem"
+import { createWalletClient, custom, createPublicClient, defineChain, parseEther, formatEther } from "https://esm.sh/viem";
+import { contractAddress, abi } from "./constants.js";
 
-const connectButton = document.getElementById('connect');
+const connectButton = document.getElementById('connect-btn');
+const fundButton = document.getElementById('fund-btn');
+const ethAmountInput = document.getElementById('ethAmount');
+const balanceButton = document.getElementById('balance-btn');
 
-let walletClient
+
+let walletClient;
+let publicClient;
 
 const connect = async () => {
     if (typeof window.ethereum !== 'undefined') {
         walletClient = createWalletClient({
             transport: custom(window.ethereum)
         })
-        await walletClient.requestAddresses()
+        await walletClient.requestAddresses();
         connectButton.innerHTML = "Connected!";
     }
     else {
@@ -17,6 +23,71 @@ const connect = async () => {
     }
 }
 
+const fund = async () => {
+    const ethAmount = ethAmountInput.value;
+    console.log(`Fund Amount: ${ethAmount}`);
+
+    if (typeof window.ethereum !== 'undefined') {
+        walletClient = createWalletClient({
+            transport: custom(window.ethereum)
+        });
+        const [connectedAccounts] = await walletClient.requestAddresses();
+        const currentChain = await getCurrentChain(walletClient)
+        connectButton.innerHTML = "Connected!";
+
+        publicClient = createPublicClient({
+            transport: custom(window.ethereum)
+        });
+        const { request } = await publicClient.simulateContract({
+            address: contractAddress,
+            abi: abi,
+            functionName: "fund",
+            account: connectedAccounts,
+            chain: currentChain,
+            value: parseEther(ethAmount),
+        });
+        
+        const hash = await walletClient.writeContract(request);
+        console.log(hash);
+
+    }
+    else {
+        alert("Metamask is not installed");
+    }
+}
+
+async function getCurrentChain(client) {
+    const chainId = await client.getChainId()
+    const currentChain = defineChain({
+        id: chainId,
+        name: "Custom Chain",
+        nativeCurrency: {
+            name: "Ether",
+            symbol: "ETH",
+            decimals: 18,
+        },
+        rpcUrls: {
+            default: {
+                http: ["http://localhost:8545"],
+            },
+        },
+    })
+    return currentChain;
+}
+
+const getBalance = async () => {
+    if (typeof window.ethereum !== 'undefined') {
+        publicClient = createPublicClient({
+            transport: custom(window.ethereum)
+    });
+    const balance = await publicClient.getBalance({
+        address: contractAddress
+    });
+    console.log(formatEther(balance));
+    
+    }
+}
+
 connectButton.onclick = connect;
-
-
+fundButton.onclick = fund;
+balanceButton.onclick = getBalance;
